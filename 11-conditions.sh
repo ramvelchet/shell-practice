610eef59d4312c41f28caed2ac7b45b265d8598e
ramvelcheti <ramwalfox@gmail.com>
2025-10-10 02:03:07 +0530

    session-06

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -3,8 +3,8 @@
 echo "please enter the number:"
 read NUMBER
 
-if [(($NUMBER %2)) -eq 0]; then
-    echo "given number $NUMBER is even"
+if [ $(($NUMBER % 2)) -eq 0 ]; then
+    echo "given number $NUMBER is EVEN"
 else 
     echo "Given number $NUMBER is ODD"
 fi
