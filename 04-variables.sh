420ce2db39058ec0d3d0d9f7224cc6f3d5534d64
ramvelcheti <ramwalfox@gmail.com>
2025-10-09 16:26:59 +0530

    variable

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1:: welcome to Alaska $PERSON2"
+echo  "$PERSON2:: thank you $PERSON1, finally not on zoom website"
+echo  "$PERSON1:: so, do we have a deal"
+echo  "$PERSON2:: yes, if you make ukraine joins Russia"
