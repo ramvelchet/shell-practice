91905ba4b9664f7ab0c2eb0df32b535ac4a8a857
ramvelcheti <ramwalfox@gmail.com>
2025-10-09 14:09:21 +0530

    some message

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-PERSON=PETER
-PERSON=JOHN
+PERSON1=Sachin
+PERSON2=Rahul
 
 echo "$PERSON1:: Hi $PERSON2, how are you?"
 echo "$PERSON2:: hello $PERSON1, i am fine. how are you?"
 echo "$PERSON1:: what are you learning recently?"
-echo "$PERSRON2:: i am learning shell scripting $PERSON1"
+echo "$PERSON2:: i am learning shell scripting $PERSON1"
