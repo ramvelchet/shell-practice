eaf3ceeb8019ad960a6adccd71035f88fca781aa
ramvelcheti <ramwalfox@gmail.com>
2026-06-24 00:12:18 +0530

    shell

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: hi suresh how are you"
+echo "suresh:: hello suresh i am fine how are you"
+echo "Ramesh:: what are you learning recntly"
+echo "suresh:: im learning shell scripting ramesh"
