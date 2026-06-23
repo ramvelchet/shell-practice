41531c59fd6f467d2eac1ede26d95b7dfd91e792
ramvelcheti <ramwalfox@gmail.com>
2026-06-24 00:25:22 +0530

    shell

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=john
+PERSON2=Ram
+
+echo "$PERSON2:: hi $PERSON2 how are you"
+echo "$PERSON1:: hello $PERSON2 i am fine how are you"
+echo "$PERSON2:: what are you learning recntly"
+echo "$PERSON1:: im learning shell scripting $PERSON1"
