850b2b42c7db199f244b4c3eab363b15c6f1328b
ramvelcheti <ramwalfox@gmail.com>
2026-06-24 20:21:42 +0530

    shell

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: welcome to Alaska $PERSON2"
+echo "$PERSON2:: thank you $PERSON1 , finally not on zoom webnar"
+echo "$PERSON1:: so, do u have deal"
+echo "$PERSON2:: yes if you make joins russia"
