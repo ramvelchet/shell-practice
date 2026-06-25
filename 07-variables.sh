1cc57d6d30455853b56abfaab608d54eb34924b4
ramvelcheti <ramwalfox@gmail.com>
2026-06-25 13:30:44 +0530

    shell

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-DATE=$(date)
+#DATE=$(date)
 
-echo "Timestamp executed: $DATE"
+START_TIME=$(date +%s)
+
+sleep 10 &
+END_TIME=$(date +%s)
+TOTAL_TIME=$($END_TIME-$START_TIME)
+
+echo "Sceipt time executed in: $TOTAL_TIME Seconds"
