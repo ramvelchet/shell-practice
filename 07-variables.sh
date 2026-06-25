b3e752e1c386a69b8b861c2c67a8c68236ba594d
ramvelcheti <ramwalfox@gmail.com>
2026-06-25 13:33:13 +0530

    shell

--- 07-variables.sh
+++ 07-variables.sh
@@ -6,6 +6,6 @@ START_TIME=$(date +%s)
 
 sleep 10 &
 END_TIME=$(date +%s)
-TOTAL_TIME=$($END_TIME-$START_TIME)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
 
 echo "Sceipt time executed in: $TOTAL_TIME Seconds"
